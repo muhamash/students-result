c5197306a081d9978ce641b86631d0d549cd9804
muhamash <ashraful@xpeedlab.tech>
2024-01-29 02:01:19 +0600

    header done

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -9,7 +9,7 @@ const CustomButton = (props:ButtonProps ) =>
     const { name, bgColor } = props;
     return (
         <button
-            className={`${bgColor} px-5 py-2 text-white text-[16px] rounded-[44px]`}>
+            className={`${bgColor} px-5 py-2 text-white text-[16px] rounded-[44px] shadow-md hover:scale-105 transition-all duration-200`}>
             {name}
         </button>
     );
--- src/components/head/Header.tsx
+++ src/components/head/Header.tsx
@@ -1,10 +1,34 @@
+import CustomButton from '../buttons/Button';
 import Nav from '../nav/Nav';
 
-const Header = () => {
+const Header = () =>
+{
     return (
-        <div className='bg-green-700 w-screen'>
+        <div className='bg-[radial-gradient(50%_50%_at_50%_50%,#17956D_0%,#0F684C_100%)] pt-32 pb-20 -mt-[92px] md:-mt-[118px]'>
             <div>
-                <Nav/>
+                <div className='py-6'>
+                    <Nav />
+                </div>
+                <div className="container">
+                <div className="grid md:grid-cols-2 items-center w-10/12 mx-auto">
+                    <img
+                        className="md:order-2 object-cover ml-auto animate-updown"
+                        src="/src/assets/hero.svg"
+                        width="500px"
+                        height="500px"
+                        alt="Banner"
+                    />
+                    <div>
+                        <h1
+                            className="text-4xl lg:text-[56px] font-bold leading-[1.1] mb-8"
+                        >
+                            The Future of Learning starts with students at the
+                            center
+                        </h1>
+                        <CustomButton name='Learn more' bgColor='bg-[#038C61]'/>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     );
